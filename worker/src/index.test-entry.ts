a64632f593d2b8daffde752fc6aa1ed5ad2f2c7e
cryogenized-spec <cryogenized@gmail.com>
2026-09-21 19:30:04 +0200

    ClickUp tests: remove unused RPC harness export

--- worker/src/index.test-entry.ts
+++ worker/src/index.test-entry.ts
@@ -1,5 +1,3 @@
 export { default, AutonomyEngine, GoogleOAuthVault, ClickUpOAuthVault, RoutineRunWorkflow } from './entry';
 export { TestAutonomyEngine } from './autonomy/engine.test-harness';
 export { TestGoogleOAuthVault } from './google/oauth-vault.test-harness';
-
-export { TestClickUpOAuthVault } from './clickup/oauth-vault.test-harness';
